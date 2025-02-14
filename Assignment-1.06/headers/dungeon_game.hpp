a612d7cce7ab827dece57373ebc104ea3982db8c
Eitan <eitantuchin@gmail.com>
2025-02-14 17:24:53 -0600

    Added telport feature mapped to g key

--- Assignment-1.06/headers/dungeon_game.hpp
+++ Assignment-1.06/headers/dungeon_game.hpp
@@ -68,6 +68,7 @@ extern const cell_t CORRIDOR_CELL;
 extern const cell_t UPWARD_STAIRS_CELL;
 extern const cell_t DOWNWARD_STAIRS_CELL;
 extern const cell_t PLAYER_CELL;
+extern const cell_t POINTER_CELL;
 
 typedef enum {
     UP,
@@ -132,5 +133,7 @@ std::string getMonsterPositionString(int monsterIndex);
 void displayMonsterList(void);
 void updateFogMap(void);
 void printCharacter(int x, int y);
-void teleportPlayer(void);
+void teleportPlayer(bool randomTeleport);
 void moveTargetingPointer(int key);
+void initTargetingPointer(void);
+pair<int, int> getPointerCellPosition(void);
